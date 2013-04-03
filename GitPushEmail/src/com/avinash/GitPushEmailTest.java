0f420d4e7f2a9d2b19416ce6a1a6c459fb45b262
avinash <Avinash.Koneru@catalinamarketing.com>
2013-04-03 12:45:04 -0400

    testing again

--- GitPushEmail/src/com/avinash/GitPushEmailTest.java
+++ GitPushEmail/src/com/avinash/GitPushEmailTest.java
@@ -4,6 +4,6 @@ public class GitPushEmailTest {
 
 	public static void main(String[] args){
 		System.out.println("Testing Hooks");
-		System.out.println("Testing Hooks with more email ids.?");
+		System.out.println("Testing Hooks with more email ids?");
 	}
 }
