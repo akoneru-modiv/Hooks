cad6551ba12e6a060356d61f1fe7c1bff3782719
avinash <Avinash.Koneru@catalinamarketing.com>
2013-04-03 11:35:15 -0400

    added more email ids to mailing list

--- GitPushEmail/src/com/avinash/GitPushEmailTest.java
+++ GitPushEmail/src/com/avinash/GitPushEmailTest.java
@@ -4,5 +4,6 @@ public class GitPushEmailTest {
 
 	public static void main(String[] args){
 		System.out.println("Testing Hooks");
+		System.out.println("Testing Hooks with more email ids.");
 	}
 }
